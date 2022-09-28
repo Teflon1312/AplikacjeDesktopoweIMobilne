2634ed6b642fa69347b5c95a0cd8e0d7a18c9fb5
Michal <fdsgs>
2022-09-28 09:04:22 +0200

    l

---
+++ Pierwszy projekt/FirstWinFormsApp/MainForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace FirstWinFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFirst = new System.Windows.Forms.Button();
+            this.buttonSecond = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonFirst
+            // 
+            this.buttonFirst.Font = new System.Drawing.Font("Segoe UI", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.buttonFirst.Location = new System.Drawing.Point(112, 38);
+            this.buttonFirst.Name = "buttonFirst";
+            this.buttonFirst.Size = new System.Drawing.Size(216, 114);
+            this.buttonFirst.TabIndex = 0;
+            this.buttonFirst.Text = "Przycisk";
+            this.buttonFirst.UseVisualStyleBackColor = true;
+            // 
+            // buttonSecond
+            // 
+            this.buttonSecond.Font = new System.Drawing.Font("Segoe UI", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.buttonSecond.Location = new System.Drawing.Point(475, 200);
+            this.buttonSecond.Name = "buttonSecond";
+            this.buttonSecond.Size = new System.Drawing.Size(346, 114);
+            this.buttonSecond.TabIndex = 1;
+            this.buttonSecond.Text = "baton";
+            this.buttonSecond.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.buttonSecond.UseVisualStyleBackColor = true;
+            this.buttonSecond.Click += new System.EventHandler(this.buttonSecond_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 457);
+            this.Controls.Add(this.buttonSecond);
+            this.Controls.Add(this.buttonFirst);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Pierwszy program okienkowy";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.Button buttonSecond;
+    }
+}
+
---
+++ Pierwszy projekt/FirstWinFormsApp/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWinFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFirst_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Witaj w pierwszym programie WinForms");
+        }
+
+        private void buttonSecond_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
